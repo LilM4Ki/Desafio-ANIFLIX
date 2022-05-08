116eed23de020fde181742eda42f26a28a926999
LilM4Ki <renanmakiya@hotmail.com>
2022-05-08 16:15:51 -0300

    Aprimorar e corrigir slides principais, colocando modais nos botões e cartaz de animes

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -7,10 +7,35 @@ $('.owl-carousel').owlCarousel({
             items:2
         },
         600:{
-            items:3
+            items:4
         },
         1000:{
             items:8
         }
     }
 })
+
+
+var stopVideo = () => {
+    var iframe = document.querySelector('#video')
+    if (iframe) {
+        var iframeSrc = iframe.src;
+        iframe.src = iframeSrc;
+    }
+};
+
+var stopVideo2 = () => {
+    var iframe2 = document.querySelector('#video2')
+    if (iframe2) {
+        var iframeSrc2 = iframe2.src;
+        iframe2.src = iframeSrc2;
+    }
+};
+
+var stopVideo3 = () => {
+    var iframe3 = document.querySelector('#video3')
+    if (iframe3) {
+        var iframeSrc3 = iframe3.src;
+        iframe3.src = iframeSrc3;
+    }
+};
